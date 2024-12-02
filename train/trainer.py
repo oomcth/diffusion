1003a05f39babe504d6f07f477a312adcc7e1abd
Mathis Scheffler <mathisscheffler@MacBook-Pro-de-Mathis-2.local>
2024-12-02 17:05:30 +0100

    trainer changes

--- train/trainer.py
+++ train/trainer.py
@@ -15,7 +15,7 @@ from data_utils.data_utils import DatasetPreprocess
 from datasets import load_dataset
 from transformers import CLIPTextModel, CLIPTokenizer
 from data_utils.data_utils import get_grounding_loss_by_layer, get_word_idx
-
+import torch.functional as F
 
 # set better device
 if torch.cuda.is_available():
@@ -33,7 +33,9 @@ weight_decay = 1e-4
 weight_dtype = torch.float32
 first_epoch = 0
 last_epoch = 100
-
+train_layers_ls = list(range(40))
+token_loss_scale = 1
+pixel_loss_scale = 1
 
 # initialize Modules and other training classes
 model: nn.Module = UNet2DConditionModel(
@@ -88,6 +90,8 @@ vae.to(device, dtype=weight_dtype)
 
 
 # train
+step_cnt = 0
+global_step = 0
 for epoch in range(first_epoch, last_epoch):
     model.train()
     train_loss = 0.0
@@ -140,39 +144,40 @@ for epoch in range(first_epoch, last_epoch):
             grounding_loss_dict = {}
 
             # mid_8, up_16, up_32, up_64 for sd14
-            for train_layer in train_layers_ls:
-                layer_res = int(train_layer.split("_")[1])
+            for layer_res in train_layers_ls:
 
-                attn_loss_dict = \
-                    get_grounding_loss_by_layer(
+                attn_loss_dict = get_grounding_loss_by_layer(
                     _gt_seg_list=gt_seg_ls,
                     word_token_idx_ls=word_token_idx_ls,
                     res=layer_res,
-                    input_attn_map_ls=attn_dict[train_layer],
-                    is_training_sd21=is_training_sd21,
+                    input_attn_map_ls=attn_dict[layer_res],
+                    is_training_sd21=False,
                 )
 
                 layer_token_loss = attn_loss_dict["token_loss"]
                 layer_pixel_loss = attn_loss_dict["pixel_loss"]
 
-                grounding_loss_dict[f"token/{train_layer}"] = layer_token_loss
-                grounding_loss_dict[f"pixel/{train_layer}"] = layer_pixel_loss
+                grounding_loss_dict[f"token/{layer_res}"] = layer_token_loss
+                grounding_loss_dict[f"pixel/{layer_res}"] = layer_pixel_loss
 
                 token_loss += layer_token_loss
                 pixel_loss += layer_pixel_loss
 
-            grounding_loss = token_loss_scale * token_loss + pixel_loss_scale * pixel_loss
+            grounding_loss = token_loss_scale * token_loss
+            grounding_loss += pixel_loss_scale * pixel_loss
 
-            denoise_loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+            denoise_loss = F.mse_loss(model_pred.float(),
+                                      noise.float(),
+                                      reduction="mean")
 
             # get learing rate
-            lr = lr_scheduler.get_last_lr()[0]
+            lr = scheduler.get_last_lr()[0]
 
             step_cnt += 1
 
             loss_dict = {
-                "step/step_cnt" : step_cnt,
-                "lr/learning_rate" : lr,
+                "step/step_cnt": step_cnt,
+                "lr/learning_rate": lr,
                 "train/token_loss_w_scale": token_loss_scale * token_loss,
                 "train/pixel_loss_w_scale": pixel_loss_scale * pixel_loss,
                 "train/denoise_loss": denoise_loss,
@@ -184,61 +189,15 @@ for epoch in range(first_epoch, last_epoch):
 
             loss = denoise_loss + grounding_loss
 
-            # we reset controller twice because we use grad_checkpointing, which will have additional forward during the backward process
             controller.reset()
 
-            # Gather the losses across all processes for logging (if we use distributed training).
-            avg_loss = accelerator.gather(loss.repeat(args.train_batch_size)).mean()
-                            
-            train_loss += avg_loss.item() / args.gradient_accumulation_steps
+            train_loss += loss.item()
 
             # Backpropagate
-            accelerator.backward(loss)
-            if accelerator.sync_gradients:
-                accelerator.clip_grad_norm_(unet.parameters(), args.max_grad_norm)
             optimizer.step()
-            lr_scheduler.step()
+            scheduler.step()
             optimizer.zero_grad()
 
-        # Checks if the accelerator has performed an optimization step behind the scenes
-        if accelerator.sync_gradients:
-            if args.use_ema:
-                ema_unet.step(unet.parameters())
-            progress_bar.update(1)
+        global_step += 1
+        train_loss = 0.0
 
-            global_step += 1
-            accelerator.log({"train_loss": train_loss}, step=global_step)
-            train_loss = 0.0
-
-            # save checkpoint 
-            if global_step % args.checkpointing_steps == 0:
-                if accelerator.is_main_process:
-                    # _before_ saving state, check if this save would set us over the `checkpoints_total_limit`
-                    if args.checkpoints_total_limit is not None:
-                        checkpoints = os.listdir(args.output_dir)
-                        checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
-                        checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
-
-                        # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
-                        if len(checkpoints) >= args.checkpoints_total_limit:
-                            num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
-                            removing_checkpoints = checkpoints[0:num_to_remove]
-
-                            logger.info(
-                                f"{len(checkpoints)} checkpoints already exist, removing {len(removing_checkpoints)} checkpoints"
-                            )
-                            logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
-
-                            for removing_checkpoint in removing_checkpoints:
-                                removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
-                                shutil.rmtree(removing_checkpoint)
-
-                    save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
-                    accelerator.save_state(save_path)
-                    logger.info(f"Saved state to {save_path}")
-
-        logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
-        progress_bar.set_postfix(**logs)
-
-        if global_step >= args.max_train_steps:
-            break
